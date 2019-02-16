aa27ec23a2cb343320cb38a0d08f3c30d57f07a4
Alexey Shashin <shashin.aeh@phystech.edu>
2019-02-16 13:35:58 +0300

    complete invertor problem

--- problems/00_invertor/pset00_invertor.v
+++ problems/00_invertor/pset00_invertor.v
@@ -6,16 +6,12 @@
 *   Problem set #00
 */
 
-module pset00_invertor(
-/*
-*   Problem 2/5:
-*   describe ports here
-*/
+module pset00_invertor
+(
+    input  wire in,
+    output wire out
 );
 
-/*
-*   Problem 2/5:
-*   write NOT logic here
-*/
+assign out = ~in;
 
 endmodule
